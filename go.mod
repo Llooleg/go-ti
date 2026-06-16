8dae1b0e7ef6aa931fe5e8ade14055040d958b1a
Llooleg <helgilebed@gmail.com>
2026-06-16 19:57:07 +0300

    added vt url scan

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-ti
+
+go 1.26.4
+
+require (
+	github.com/VirusTotal/vt-go v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/thedevsaddam/gojsonq/v2 v2.5.2 // indirect
+)
--- hello-world.go
+++ hello-world.go
@@ -1,12 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/VirusTotal/vt-go"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	a := 1
-	b := 2
-	c := a + b
-	fmt.Println(c)
-	//fmt.Println(1+1)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err) // %v покажет реальную причину
+	}
+	APIKey := os.Getenv("VTKEY")
+	if APIKey == "" {
+		log.Fatal("VTKEY is not set in the environment variables.")
+	}
+	client := vt.NewClient(APIKey)
+	fmt.Print("Enter a link here: ")
+	var link string
+	fmt.Scanln(&link)
+	fmt.Println("You entered:", link)
+	fmt.Println("Scanning the link...")
+	encodedURL := base64.RawURLEncoding.EncodeToString([]byte(strings.TrimSpace(link)))
+
+	apiPath := fmt.Sprintf("https://www.virustotal.com/api/v3/urls/%s", encodedURL)
+
+	vtURL, err := url.Parse(apiPath)
+	if err != nil {
+		log.Fatalf("Invalid URL path: %v", err)
+	}
+
+	obj, err := client.GetObject(vtURL)
+	if err != nil {
+		log.Fatalf("Failed to retrieve report: %v", err)
+	}
+
+	stats, err := obj.Get("last_analysis_stats")
+	if err != nil {
+		fmt.Println("Could not find stats for this link.")
+	} else {
+		fmt.Printf("Analysis Stats: %v\n", stats)
+		fmt.Print("Press Enter to continue...")
+		fmt.Scanln()
+	}
+
 }
